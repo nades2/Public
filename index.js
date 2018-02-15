8ebec215667979af9e953551b0fa2de2952bee45
nades2 <stephane.nadeau@gmail.com>
2018-02-14 21:38:29 -0500

    added user obj

---
+++ index.js
@@ -0,0 +1,45 @@
+var connection = 'Enterprise-customer';
+
+
+var userList = [
+    {
+        user1 : ''
+    }
+];
+var merchantList = [
+    {
+        merchant : '773636',
+        merchant_name : 'sub',
+        merchant_type: '',
+        merchant_chain : '',
+        merchant_sub : ''
+
+    },
+];
+
+var userObj = [
+    {
+        "connection":connection,
+        "email":"kmcmurtyr0@telegraph.co.uk",
+        "username":"username-1",
+        "name":"username-1",
+        "password":"poutine+1",
+        "user_metadata": {
+            "first_name":"Kerry",
+            "last_name":"McMurtyr",
+            "locale":"en"
+        },
+        "app_metadata": {
+            "applicationRoles": [
+                "PortailTestGroup3"
+            ],
+            "scd": {
+                "merchant":"195750",
+                "merchant_name":"M: CIE MARYJANE FLEURY",
+                "merchant_type":"M",
+                "merchant_chain":"626978",
+                "merchant_sub":"569992"
+            }
+        }
+    }
+];
